91d8fac6dbbfb219dd58f57c182fc7163234bf33
gkarthik2710 <karthikkarthik47162@gmail.com>
2025-02-06 23:41:58 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,13 @@
+// let a=10;
+// let b=40;
+// let c=a+b;
+// let flag= true;
+// let name;
+// let cost=null;
+// document.writeln(typeof a );
+// document.write(typeof cost);
+let name=prompt("Enter Your Name");
+let flag =confirm("Print Data")
+if(flag=true){
+document.write(name);
+}
